7289c591c350a38069ff646ea55213737e7825e7
orel maxim <tel0509891255@gmail.com>
2021-12-22 20:29:04 +0300

    22.12

--- Wpf_Task4/MainWindow.xaml.cs
+++ Wpf_Task4/MainWindow.xaml.cs
@@ -25,12 +25,90 @@ namespace Wpf_Task4
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void button_Dollars_Click_1(object sender, RoutedEventArgs e)
         {
+
             double rateDollar = Convert.ToDouble(rate.Text);
             double sumDollar = Convert.ToDouble(sum.Text);
-            double rezDollar = rateDollar * sumDollar;
-            resSum.Text = rezDollar.ToString();
+            double rezRuble = rateDollar * sumDollar;
+            resSum.Text = rezRuble.ToString();
+        }
+
+        private void button_Euro_Click(object sender, RoutedEventArgs e)
+        {
+            double rateEuro = Convert.ToDouble(rate1.Text);
+            double sumEuro = Convert.ToDouble(sum1.Text);
+            double rezRuble = rateEuro * sumEuro;
+            resSum1.Text = rezRuble.ToString();
+        }
+
+        private void button_Hryvnia_Click(object sender, RoutedEventArgs e)
+        {
+            double rateHryvnia = Convert.ToDouble(rate2.Text);
+            double sumHryvnia = Convert.ToDouble(sum2.Text);
+            double rezRuble = rateHryvnia * sumHryvnia;
+            resSum2.Text = rezRuble.ToString();
+        }
+
+        private void button_Dram_Click(object sender, RoutedEventArgs e)
+        {
+            double rateDram = Convert.ToDouble(rate3.Text);
+            double sumDram = Convert.ToDouble(sum3.Text);
+            double rezRuble = rateDram * sumDram;
+            resSum3.Text = rezRuble.ToString();
+        }
+
+
+
+
+        private void buttonInches_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxInches != null && textBoxInchesResult != null)
+            {
+                double inches = double.Parse(textBoxInches.Text);
+
+                double result = inches * 0.0254;
+
+                textBoxInchesResult.Text = Convert.ToString(Math.Round(result, 2));
+            }
+
+        }
+
+        private void buttonFt_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxFt != null && textBoxFtResult != null)
+            {
+                double ft = double.Parse(textBoxFt.Text);
+
+                double result = ft / 3.2808;
+                
+                textBoxFtResult.Text = Convert.ToString(Math.Round(result, 2));
+            }
+
+        }
+
+        private void buttonMile_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxMile != null && textBoxMileResult != null)
+            {
+                double mile = double.Parse(textBoxMile.Text);
+
+                double result = mile * 1609.34;
+
+                textBoxMileResult.Text = Convert.ToString(result);
+            }
+        }
+
+        private void buttonVerst_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxVerst != null && textBoxVerstResult != null)
+            {
+                double verst = double.Parse(textBoxVerst.Text);
+
+                double result = verst * 1066.8;
+
+                textBoxVerstResult.Text = Convert.ToString(result);
+            }
         }
     }
 }
